3417035b8d7db1229f6523efbc2807fa1f501c31
Chris Jones <chrisjones0099@hotmail.com>
2012-03-08 10:37:20 +0000

    jQuery plugin

--- mesa.js
+++ mesa.js
@@ -2,67 +2,75 @@ window.mesa = window.mesa || {};
 
 mesa.Util = {
 
-	defaults: function(obj) {
+    defaults: function(obj) {
 
-		var args = Array.prototype.slice.call(arguments, 1)
+        var args = Array.prototype.slice.call(arguments, 1)
 
-		for (var i = args.length - 1; i >= 0; i--) {
-			for (var prop in args[i]) {
-	        	if (obj[prop] == null) obj[prop] = args[i][prop];
-	      	}
-		};
-	    return obj;
-	}
+        for (var i = args.length - 1; i >= 0; i--) {
+            for (var prop in args[i]) {
+                if (obj[prop] == null) obj[prop] = args[i][prop];
+            }
+        };
+        return obj;
+    }
 
 };
 
 mesa.Core = (function($, util){
 
-	var defaults = {
-		table: 'table tbody',
-		row: 'tr',
-		col: 'td'
-	};
+    var defaults = {
+        root: 'root tbody',
+        row: 'tr',
+        col: 'td'
+    };
 
-	function cols(row, options) {
+    function cols(row, options) {
 
-		var c = [];
-		$(options.col, row).each(function(i, e) {
-			c.push($(e).text());
-		});
-		return c;
-	}
+        var c = [];
+        $(options.col, row).each(function(i, e) {
+            c.push($(e).text());
+        });
+        return c;
+    }
 
-	function rows(table, options) {
+    function rows(root, options) {
 
-		var r = [];
-		$(options.row, table).each(function(i, e) {
-			r.push(cols($(e), options));
-		});
-		return r;
-	}
+        var r = [];
+        $(options.row, root).each(function(i, e) {
+            r.push(cols($(e), options));
+        });
+        return r;
+    }
 
-	return {
+    return {
 
-		load: function(options) {
+        load: function(options) {
 
-			util.defaults(options, defaults);
+            var o = util.defaults(options || {}, defaults);
+            return rows($(o.root), o);
+        },
 
-			return rows($(options.table), options);
-		}
-	};	
+        loadFromQuery: function(query, options) {
+            
+            var o = util.defaults(options || {}, defaults);
+            return rows(query, o);
+        }
+    };  
 
 })(jQuery, mesa.Util);
 
-mesa.Plugin = (function($) {
+mesa.Plugin = (function($, core) {
 
-	return {
+    return {
+        integrateJQuery: function() {
+            
+            $.fn.mesa = function(options) {
+                
+                return core.loadFromQuery(this, options);
+            }
+        }
+    }
 
-		integrateJQuery: function() {
-			
-		}
-	}
-
-})(jQuery);
+})(jQuery, mesa.Core);
 
 mesa.Plugin.integrateJQuery();
--- specs/markup_as_source.spec.js
+++ specs/markup_as_source.spec.js
@@ -4,11 +4,41 @@ describe("loading from a markup (set of divs and spans) containing data rows", f
 	var expectedRows = $(selector).children().length;
 
 	var data = mesa.Core.load({ 
-								 table : selector,
+								 root : selector,
 								 row : 'div',
 								 col : 'span'
 								});
 
+
+	it("should provide an array containing the same number of rows", function() {
+
+		expect(data.length).to.be(expectedRows);
+	});
+
+
+	it("should provide each row array with the same number of columns", function() {
+		
+		function expectRowToHaveExpectedColumns(index) {
+			
+			var expectedColumns = $('tr:eq('+index+')', selector).children().length;
+			expect(data[index].length).to.be(expectedColumns);	
+		}
+		
+		for (var i = 0; i < expectedRows.length; i++) {
+			expectRowToHaveExpectedColumns(i);
+		};
+	});
+});
+
+describe("loading from a markup (set of divs and spans) containing data rows with jQuery", function(){
+
+	var selector = '#data';
+	var expectedRows = $(selector).children().length;
+
+	var data = $(selector).mesa({
+								 row : 'div',
+								 col : 'span'
+								});
 
 	it("should provide an array containing the same number of rows", function() {
 
